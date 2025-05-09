16243681433fa56266274ed492d95636f8b56ca8
GamingLuke1337 <lkswaltr299@icloud.com>
2025-05-09 20:35:32 +0200

    update

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,14 +1,12 @@
---#### FX Information ####--
-fx_version   'cerulean'
-use_fxv2_oal 'yes'
-lua54        'yes'
-game         'gta5'
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
 
---#### Resource Information ####--
-name         'NPC Reduced Control'
-version      '1.2'
-description  'Powered by O:SX Studios'
+author 'GamingLuke1337'
+description 'NPC Control'
 
-client_script 'client/cl.lua'
+version '1.0.0'
 
-server_script 'server/sv.lua'
+client_script 'client.lua'
+server_script 'update.lua'
+shared_script '@ox_lib/init.lua'
---
+++ update.lua
@@ -0,0 +1,1 @@
+lib.versionCheck('GamingLuke1337/NPC-Control')
